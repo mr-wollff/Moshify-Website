019fb87f5ce62c08bd65de3d0419902ca312150d
mr-wollff <145775920+mr-wollff@users.noreply.github.com>
2024-01-03 21:54:35 +0300

    First Commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const collapsibles = document.querySelectorAll(".collapsible");
+collapsibles.forEach((item) =>
+  item.addEventListener("click", function () {
+    console.log("Clicked!");
+    this.classList.toggle("collapsible--expanded");
+  })
+);
